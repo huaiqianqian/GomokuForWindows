249fd318ed3060379b409c1dbcdacbb4b13e0857
huaiqian <96475524+huaiqianqian@users.noreply.github.com>
2025-03-30 04:31:18 +0800

    Add files via upload

--- gomoku.h
+++ gomoku.h
@@ -2,12 +2,11 @@
 #define _5ZIQI_
 
 #define MYSIZE 16
-// 0 is the border so the real size is MYSIZE - 1
-
 #define getchess(is_c) (is_c?'O':'X')
 
 int checkWin(int x, int y, char gomoku[MYSIZE][MYSIZE], char flagWin);
 void gomoku_show(char gomoku[][MYSIZE]);
 void gomoku_init(char gomoku[][MYSIZE]);
 void gomoku_step(char gomoku[][MYSIZE], int* x, int* y, int is_c);
+
 #endif
